ec24e9fb8f1a48792577f5606794c92672a92b62
Muhammad Eri Setyawan <erisetyawan34@gmail.com>
2024-09-24 21:00:10 +0700

    finishing update contact api

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -3,7 +3,7 @@ import { Contact, User } from "@prisma/client";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { PrismaService } from "../common/prisma.service";
 import { ValidationService } from "../common/validation.service";
-import { ContactResponse, CreateContactRequest } from "../model/contact.model";
+import { ContactResponse, CreateContactRequest, UpdateContactRequest } from "../model/contact.model";
 import { Logger } from 'winston';
 import { ContactValidation } from "./contact.validation";
 
@@ -35,18 +35,38 @@ export class ContactService {
     }
 
     async get(user: User, contactId: number): Promise<ContactResponse> {
-        const contact = await this.prismaService.contact.findFirst({
-            where: ({
-                username: user.username,
-                id: contactId,
-            })
+        const contact = await this.checkContactMustExists(user.username, contactId);
+        return this.toContactResponse(contact);
+    }
+
+    async update(user: User, request: UpdateContactRequest): Promise<ContactResponse> {
+        const updateRequest = this.validationService.validate(ContactValidation.UPDATE, request);
+        let contact = await this.checkContactMustExists(user.username, updateRequest.id);
+        
+        contact = await this.prismaService.contact.update({
+            where: {
+                id: contact.id,
+            },
+            data: updateRequest,
         })
+
+        return this.toContactResponse(contact);
+        
+    }
+
+    async checkContactMustExists(username: string, contacId: number): Promise<Contact> {
+        const contact = await this.prismaService.contact.findFirst({
+            where: {
+                username: username,
+                id: contacId,
+            },
+        });
 
         if (!contact) {
             throw new HttpException('Contact is not found', 404);
         }
 
-        return this.toContactResponse(contact);
+        return contact;
     }
 
     toContactResponse(contact: Contact): ContactResponse {
